e4b01efe9875542e533d6755dbaaddc7ca7059d9
iancu_robert <robert.iancu@zupria.com>
2024-09-11 18:47:03 +0300

    Create Contacts page layout

--- src/app/contacts/components/contacts/contacts.component.ts
+++ src/app/contacts/components/contacts/contacts.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ContactDetailsComponent } from '../contact-details/contact-details.component';
+import { ContactFiltersComponent } from '../contact-filters/contact-filters.component';
+import { ContactListComponent } from '../contact-list/contact-list.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [ContactDetailsComponent, ContactFiltersComponent, ContactListComponent, MatButtonModule],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrl: './contacts.component.scss',
 })
-export class ContactsComponent {
-
-}
+export class ContactsComponent {}
